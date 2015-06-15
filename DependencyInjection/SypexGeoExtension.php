a8bc841b9e2f666647d9cff7342849c97ecb7795
Stephan Yamilov <yamilovs@gmail.com>
2015-06-16 00:10:40 +0500

    Add configuration of sypex geo city database path

--- DependencyInjection/SypexGeoExtension.php
+++ DependencyInjection/SypexGeoExtension.php
@@ -24,5 +24,7 @@ class SypexGeoExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+
+        $container->setParameter('sypex_geo.city_database_path', $config['city_database_path']);
     }
 }
