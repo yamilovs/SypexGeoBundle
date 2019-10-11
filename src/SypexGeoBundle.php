8789b3786092206bcd125000e38b3e0b66c5862e
Yamilov Stepan <yamilovs@gmail.com>
2019-10-11 20:25:21 +0500

    Rewrited getContainerExtension method

--- src/SypexGeoBundle.php
+++ src/SypexGeoBundle.php
@@ -3,7 +3,12 @@
 namespace Yamilovs\Bundle\SypexGeoBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Yamilovs\Bundle\SypexGeoBundle\DependencyInjection\SypexGeoExtension;
 
 class SypexGeoBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new SypexGeoExtension();
+    }
 }
