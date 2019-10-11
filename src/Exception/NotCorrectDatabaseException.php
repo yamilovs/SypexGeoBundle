4441715e2e49dff6dc0c2bedd42b060fce35c489
Yamilov Stepan <yamilovs@gmail.com>
2019-10-11 18:06:17 +0500

    Dropped unused exceptions

--- src/Exception/NotCorrectDatabaseException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Yamilovs\SypexGeoBundle\Exception;
-
-class NotCorrectDatabaseException extends \RuntimeException
-{
-
-}
--- src/Exception/UnsupportedModeException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Yamilovs\SypexGeoBundle\Exception;
-
-class UnsupportedModeException extends \RuntimeException
-{
-
-}
