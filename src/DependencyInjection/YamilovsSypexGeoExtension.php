efb9e8bee7d7ac725e84716b472f94263e6ce39a
Yamilov Stepan <yamilovs@gmail.com>
2019-10-11 17:13:15 +0500

    Moved bundle files under `src` and test under `tests`

--- src/DependencyInjection/YamilovsSypexGeoExtension.php
+++ src/DependencyInjection/YamilovsSypexGeoExtension.php
@@ -23,7 +23,7 @@ class YamilovsSypexGeoExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
 
         $container->setParameter($this->getAlias() . '.database_path', $config['database_path']);
