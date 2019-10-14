1588e5091533fbb9691d6ae4e1aedc6715651c25
Yamilov Stepan <yamilovs@gmail.com>
2019-10-14 17:30:30 +0500

    Explode auth configuration to two separate nodes

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -1,46 +1,53 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Yamilovs\Bundle\SypexGeoBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Yamilovs\SypexGeo\Database\Mode;
 
-/**
- * This is the class that validates and merges configuration from your app/config files
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
- */
 class Configuration implements ConfigurationInterface
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder();
+        $treeBuilder = new TreeBuilder('yamilovs_sypex_geo');
+
         $rootNode = $treeBuilder->root('yamilovs_sypex_geo');
 
         $rootNode
             ->children()
-                ->scalarNode('mode')->defaultValue('SXGEO_FILE')->end()
-                ->scalarNode('database_path')->defaultFalse()->end()
+                ->scalarNode('mode')
+                    ->defaultValue(array_search(Mode::FILE, Mode::getModes(), true))
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                ->end()
+                ->scalarNode('database_path')
+                    ->defaultFalse()
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                ->end()
                 ->arrayNode('connection')
                     ->addDefaultsIfNotSet()
                     ->children()
                         ->arrayNode('proxy')
                             ->children()
                                 ->scalarNode('host')->defaultNull()->end()
-                                ->scalarNode('auth')->defaultNull()->end()
+                                ->integerNode('port')->defaultNull()->end()
+                                ->arrayNode('auth')
+                                    ->addDefaultsIfNotSet()
+                                    ->children()
+                                        ->scalarNode('user')->defaultNull()->end()
+                                        ->scalarNode('password')->defaultNull()->end()
+                                    ->end()
+                                ->end()
                             ->end()
                         ->end()
                     ->end()
                 ->end()
             ->end()
         ;
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
 
         return $treeBuilder;
     }
