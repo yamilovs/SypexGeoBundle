73103ab0fc5444ce529bbc90b0466e4366a5f93e
Yamilov Stepan <yamilovs@gmail.com>
2019-10-14 13:06:36 +0500

    Reformat Yamilovs\Bundle\SypexGeoBundle\Command\GetIpDataCommand

--- src/Command/GetIpDataCommand.php
+++ src/Command/GetIpDataCommand.php
@@ -1,16 +1,35 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Yamilovs\Bundle\SypexGeoBundle\Command;
 
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use ReflectionClass;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\TableCell;
+use Symfony\Component\Console\Helper\TableSeparator;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Yamilovs\SypexGeo\SypexGeo;
 
-class GetIpDataCommand extends ContainerAwareCommand
+class GetIpDataCommand extends Command
 {
-    protected function configure() {
+    /**
+     * @var SypexGeo
+     */
+    private $sypexGeo;
+
+    public function __construct(SypexGeo $sypexGeo)
+    {
+        $this->sypexGeo = $sypexGeo;
+
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
         $this
             ->setName('yamilovs:sypex-geo:get-ip-data')
             ->setDescription('Get all data about specific ip from database file')
@@ -18,22 +37,55 @@ class GetIpDataCommand extends ContainerAwareCommand
         ;
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output) {
+    protected function execute(InputInterface $input, OutputInterface $output): void
+    {
         $io = new SymfonyStyle($input, $output);
         $ip = $input->getArgument('ip');
-        $manager = $this->getContainer()->get('yamilovs.sypex_geo.manager');
-        $city_data = $manager->getCity($ip);
 
-        $io->title("Data from database file for address $ip");
-        $headers = ['Parent', 'Parameter' ,'Value'];
+        $city = $this->sypexGeo->getCity($ip, true);
+
+        $io->title(sprintf("Data from database file for address '%s'", $ip));
         $rows = [];
 
-        foreach ($city_data as $key => $value) {
-            foreach ($value as $k => $v) {
-                $rows[] = [$key, $k, $v];
+        $data = new \ArrayIterator($this->parseData($city));
+
+        while ($data->valid()) {
+            $rows[] = [new TableCell($data->key(), ['colspan' => 2])];
+            $rows[] = new TableSeparator();
+
+            foreach ($data->current() as $key => $value) {
+                $rows[] = [$key, $value];
+            }
+
+            $data->next();
+
+            if ($data->valid()) {
+                $rows[] = new TableSeparator();
             }
         }
 
-        $io->table($headers, $rows);
+        $io->table([], $rows);
+    }
+
+    private function parseData(object $class): array
+    {
+        $result = [];
+        $methods = get_class_methods($class);
+
+        foreach ($methods as $method) {
+            if (strpos($method, 'get') !== 0) {
+                continue;
+            }
+
+            $return = $class->$method();
+
+            if (is_object($return)) {
+                $result += $this->parseData($return);
+            } else {
+                $result[(new ReflectionClass($class))->getShortName()][substr($method, 3)] = $return;
+            }
+        }
+
+        return $result;
     }
 }
